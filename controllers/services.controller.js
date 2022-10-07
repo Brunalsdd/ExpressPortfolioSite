85e653b99e514ac8c973297ef28b27da00b6f659
brunalsdd <libarde3@hotmail.com>
2022-10-07 16:26:49 -0400

    page population

--- controllers/services.controller.js
+++ controllers/services.controller.js
@@ -1,7 +1,7 @@
 
 exports.render = function (req, res) {
     res.render('services', {
-        title: 'How to contact me',
-        description: 'Here you can find some contact information'
+        title: 'Services',
+        description: 'Here are some services'
     })
 };

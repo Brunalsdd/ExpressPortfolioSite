cc5bf733a4b65017a8a0ea53a72c0fcc041676de
brunalsdd <libarde3@hotmail.com>
2022-10-07 19:18:12 -0400

    some fix, services and projects view

--- controllers/aboutme.controller.js
+++ controllers/aboutme.controller.js
@@ -8,6 +8,8 @@ exports.render = function (req, res) {
     res.render('aboutme', {
         title: 'About Me',
         name: 'Bruna Donatoni',
-        description: 'I am an industrial engineer with 3 years of experience. In my work, I realized that I was not fluent in machine language, so I am studying software Engineering Technician at Centennial College. Please, for more details, see my resume in pdf.  '
+        description: 'I am an industrial engineer with 3 years of experience. In my work, I realized that I was not fluent in machine language, so I am studying software Engineering Technician at Centennial College. Please, for more details, see my resume in pdf.  ',
+        linkedin: 'https://github.com/Brunalsdd',
+        github: 'https://www.linkedin.com/in/bruna-ducceschi-donatoni-3386406b/'
     })
 };
--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -7,6 +7,11 @@ Date: Oct 05, 2022
 exports.render = function (req, res) {
     res.render('contact', {
         title: 'How to contact me',
-        description: 'Here you can find some contact information'
+        description: 'Here you can find some contact information',
+        name: 'Bruna Donatoni',
+        phone: '(647) 853-3412',
+        email: 'libarde3@hotmail.com',
+        linkedin: 'https://github.com/Brunalsdd',
+        github: 'https://www.linkedin.com/in/bruna-ducceschi-donatoni-3386406b/'
     })
 };
